e99dfa8bbbe188e2bde14b509248c4bcc84df0b5
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-05-16 03:05:02 +0100

    setting up DRF

--- drf_ecommerce/settings/base.py
+++ drf_ecommerce/settings/base.py
@@ -42,8 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+   
+   #External apps 
     'rest_framework',
+    
+    #internal apps
 ]
 
 MIDDLEWARE = [

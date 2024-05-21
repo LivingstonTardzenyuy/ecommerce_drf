a5225d9e9aa7e5d7f6c79a3e6470d82678ebf4c8
LivingstonTardzenyuy <kongnyuylivingston2@gmail.com>
2024-05-21 03:24:57 +0100

    Finsih the models.py

--- drf_ecommerce/settings/base.py
+++ drf_ecommerce/settings/base.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition
--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
 class Attribute(models.Model):
@@ -17,22 +17,35 @@ class Brand(models.Model):
     name = models.CharField(max_length=255)
 
 
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=255)
+    parent = TreeForeignKey("self", on_delete= models.PROTECT, null = True, blank = True)
+
+    class MPTTMeta:
+        order_insertion_by =['name']
+        
+    def __str__(self):
+        return self.name
+
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return (self.name + self.description)
 
+    
 class ProductLine(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock_qty = models.IntegerField()
     product = models.ManyToManyField(AttributValue)
     product_fk = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-class Category(models.Model):
-    name = models.CharField(max_length=255)
-    product = models.ManyToManyField(Product)
 
 
 class ProductImage(models.Model):
